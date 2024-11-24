184bf4c5a8e1be50de5f63898693470c3a545483
atharvkhardekar <khardekaratharv@gmail.com>
2024-11-24 11:34:22 +0530

    Updated Script for Cassandra Connection

--- connection.py
+++ connection.py
@@ -3,11 +3,16 @@ from cassandra.cluster import Cluster
 # Connect to the Cassandra database
 cluster = Cluster(['127.0.0.1'])  # Replace with 'localhost' or '127.0.0.1'
 session = cluster.connect()
+print("Connection established successfully!")
 
 # Create and use the keyspace
 session.execute("""
     CREATE KEYSPACE IF NOT EXISTS test
     WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}
 """)
-session.set_keyspace('test')
+if(session.set_keyspace('test')) :
+ print("Keyspace 'test' is now active.")
+else :
+ print("An error occurred while setting up the keyspace:")
+
 
